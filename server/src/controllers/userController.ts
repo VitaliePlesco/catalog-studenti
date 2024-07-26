d699cd58fd500cb848f6c3a8cb221d2375f07f30
vitalieplesco <v.plesco@gmail.com>
2024-07-26 19:42:42 +0300

    add refresh token route

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -2,6 +2,7 @@ import { Request, Response, } from "express";
 import { db } from "../db/index.js";
 import { user } from "../db/schema.js";
 import generateToken from "../utils/generateToken.js";
+import jwt, { JwtPayload, VerifyErrors } from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 
 
@@ -29,7 +30,8 @@ export const authUser = async (req: Request, res: Response) => {
     }
     if (userExists && passwordMatch) {
       generateToken(res, userExists.id);
-      return res.status(201).json({ user: userExists.email }).end();
+      const accessToken = jwt.sign(userExists.id, process.env.JWT_SECRET as string, { expiresIn: "15min" });
+      return res.status(201).json({ accessToken }).end();
     }
 
   } catch (error) {
@@ -41,11 +43,15 @@ export const registerUser = async (req: Request, res: Response) => {
   try {
     const { firstName, lastName, email, password } = await req.body;
 
+    if (!firstName || !lastName || !email || !password) {
+      return res.status(400).json({ message: 'All fields are required' })
+    }
+
     const userExists = await db.query.user.findFirst({
       where: (user, { eq }) => eq(user.email, email)
     })
     if (userExists) {
-      return res.status(400).json({ message: "user exists" });
+      return res.status(409).json({ message: "user exists" });
     }
 
     const hashedPassword = await bcrypt.hash(password, 12);
@@ -59,12 +65,46 @@ export const registerUser = async (req: Request, res: Response) => {
 
     if (newUser) {
       generateToken(res, newUser.id);
-      return res.status(201).json({ message: "new user succes" }).end();
+      return res.status(201).json({ message: "new user created" }).end();
     }
   } catch (error) {
     return res.status(400).json({ message: "could not create new user", error: error });
   }
 }
+
+export const refreshToken = async (req: Request, res: Response) => {
+  const cookies = req.cookies;
+
+  if (!cookies?.jwt) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const refreshToken = cookies.jwt;
+
+    jwt.verify(refreshToken, process.env.JWT_SECRET as string,
+      async (err: any, decoded: any) => {
+        if (err) {
+          return res.status(403).json({ message: "Forbiden" });
+        }
+
+        const userExists = await db.query.user.findFirst({
+          where: (user, { eq }) => eq(user.id, decoded.id)
+        });
+
+        if (!userExists) {
+          return res.status(400).json({ message: "Unauthorized" });
+        }
+        const accessToken = jwt.sign(userExists.id, process.env.JWT_SECRET as string, { expiresIn: "15min" });
+        res.json({ accessToken });
+      }
+    )
+  } catch (error) {
+    return res.status(400).json({ message: "refresh token", error: error });
+  }
+}
+
+
 
 export const logoutUser = async (req: Request, res: Response) => {
   res.clearCookie("jwt", {
--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,10 +1,11 @@
 import express from "express";
 const router = express.Router();
-import { authUser, registerUser, logoutUser, getUserProfile } from "../controllers/userController.js";
+import { authUser, registerUser, logoutUser, getUserProfile, refreshToken } from "../controllers/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 router.post("/", registerUser);
 router.post("/auth", authUser);
+router.get("/refresh", refreshToken)
 router.post("/logout", logoutUser);
 router.route("/profile").get(protect, getUserProfile);
 
